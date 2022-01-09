8f57ddaedfce2e577678a60366c52c95947bb076
Hayk Avetisyan <haykavetisyan992@gmail.com>
2022-01-10 01:18:31 +0400

    Education with Collection

---
+++ src/storage/LessonStorage.java
@@ -0,0 +1,56 @@
+package storage;
+
+import data.DataClass;
+import model.Lesson;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LessonStorage {
+    private List<Lesson> list = new LinkedList<>();
+
+
+    public void add(Lesson lesson) {
+        list.add(lesson);
+        serial();
+
+    }
+
+
+    public void printLessons() {
+        for (Lesson lesson : list) {
+            System.out.println(lesson);
+
+        }
+    }
+
+    public void deleteLessonByName(String name) {
+        for (Lesson lesson : list) {
+            if (lesson.getName().equals(name)) {
+                list.remove(lesson);
+                serial();
+                System.out.println("The lesson has been deleted");
+            }
+        }
+    }
+
+    public Lesson getLessonByName(String name) {
+        for (Lesson lesson : list) {
+            if (lesson.getName().equals(name)) {
+                return lesson;
+            }
+        }
+        return null;
+    }
+
+    public void serial() {
+        DataClass.serializeLessons(list);
+    }
+
+    public void initData() {
+        List<Lesson> lessonList = DataClass.deSerializeLessons();
+        if (lessonList != null) {
+            list = lessonList;
+        }
+    }
+}
